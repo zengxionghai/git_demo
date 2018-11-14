463f0ddca429fc64b14a712dc4a326355c8c078a
zxh <794769998@qq.com>
2018-11-14 20:28:30 +0800

    删除了两个语句

--- src/main/java/com/qf/Tesr.java
+++ src/main/java/com/qf/Tesr.java
@@ -11,7 +11,5 @@ public class Tesr {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("hello world。。。");
-        System.out.println("你好");
     }
 }
