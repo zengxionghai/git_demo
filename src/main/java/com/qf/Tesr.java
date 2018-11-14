7aef766f907ec77e37638acf74a78e9b5bea80de
zxh <794769998@qq.com>
2018-11-14 20:14:14 +0800

    Initial commit

---
+++ src/main/java/com/qf/Tesr.java
@@ -0,0 +1,12 @@
+package com.qf;
+
+/**
+ * @Author Administrator
+ * @Time 2018/11/14 20:09
+ * @Version 1.0
+ */
+public class Tesr {
+    public static void main(String[] args) {
+        System.out.println("hello world。。。");
+    }
+}
